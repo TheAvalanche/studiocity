1c10e997514d862176cf279f1849213a195636a5
Alex Kartishev <alexkartishev@gmail.com>
2015-08-19 18:26:12 +0300

    Init commit

---
+++ src/main/java/org/symphodia/studiocity/rest/TestController.groovy
@@ -0,0 +1,14 @@
+package org.symphodia.studiocity.rest
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!"
+    }
+
+}
