693ea34295cb280d67a2a52c7bb66b9ba3ef9b4a
Alex Kartishev <alexkartishev@gmail.com>
2015-08-23 18:17:44 +0300

    prepare for openshift deploy

--- src/main/groovy/org/symphodia/studiocity/Application.groovy
+++ src/main/groovy/org/symphodia/studiocity/Application.groovy
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class Application {
     static void main(String... args) {
-
         SpringApplication.run(Application.class, args)
     }
 }
