346726e3bcad81dede799c245b6a3623c2f81c2f
Alex Kartishev <alexkartishev@gmail.com>
2015-12-29 12:45:39 +0200

    Unti tests

--- src/test/groovy/org/symphodia/studiocity/rest/StudioControllerTest.groovy
+++ src/test/groovy/org/symphodia/studiocity/rest/StudioControllerTest.groovy
@@ -34,6 +34,11 @@ class StudioControllerTest extends Specification {
         sharedUser = userRepository.save(new User(email: "test@test.com", password: "pass"))
     }
 
+    def cleanup() {
+        userRepository.deleteAll()
+        studioRepository.deleteAll()
+    }
+
     def "test save and update studio"() {
         when: "saving studio"
         studioController.save(new SecurityUser(sharedUser), new Studio(name: "Test 1"))
@@ -51,6 +56,41 @@ class StudioControllerTest extends Specification {
         then:
         updatedStudio.name == "Test 2"
         updatedDbUser.studios[0].name == "Test 2"
+    }
+
+    def "test remove studio"() {
+        setup:
+        studioController.save(new SecurityUser(sharedUser), new Studio(name: "Test 1"))
+        studioController.save(new SecurityUser(sharedUser), new Studio(name: "Test 2"))
+
+        when: "checking added studios"
+        def studios = studioRepository.findAll()
+        def dbUser = userRepository.findOneByEmail("test@test.com")
+        then:
+        studios.size() == 2
+        dbUser.studios.size() == 2
+
+        when: "checking delete action"
+        studioController.remove(new SecurityUser(sharedUser), studios.find {it.name == "Test 1"})
+        def updatedStudio = studioRepository.findAll()
+        def updatedDbUser = userRepository.findOneByEmail("test@test.com")
+        then:
+        updatedStudio.size() == 1
+        updatedStudio[0].name == "Test 2"
+        updatedDbUser.studios.size() == 1
+        updatedDbUser.studios[0].name == "Test 2"
+    }
+
+    def "test find current user studios"() {
+        setup:
+        studioController.save(new SecurityUser(sharedUser), new Studio(name: "Test 1"))
+        studioController.save(new SecurityUser(sharedUser), new Studio(name: "Test 2"))
+        when: "checking added studios"
+        def studios = studioController.findByCurrentUser(new SecurityUser(sharedUser))
+        then:
+        studios.size() == 2
+        studios[0].name == "Test 1"
+        studios[1].name == "Test 2"
     }
 
 }
